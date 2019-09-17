01d2001eae3f22e0015ec6fbb092ea7fd542dc23
Blake Walsh <blakeofthepath@gmail.com>
2019-09-17 16:00:05 +0300

    Update to check ordering

--- .scripts/check_ordering.py
+++ .scripts/check_ordering.py
@@ -5,19 +5,9 @@ from num import DottedNum
 from common import print_name_if_needed
 
 
-if __name__ == '__main__':
-    files = sorted(Path('./html').glob('**/*.html'))
-
-    for file in files:
-        with file.open('r') as f:
-            lines = f.readlines()
-
-        seen = set()
-
-        segment_ids = [regex.findall(r'data-uid="[^:]+\:([a-z0-9.^-]+)"', line) for line in lines]
-        segment_ids = [s for s in segment_ids if s]
-        
-        for i, line_ids in enumerate(segment_ids):
+def check_ordering(segment_ids, file):
+    seen = set()
+    for i, line_ids in enumerate(segment_ids):
             for segment_id in line_ids:
                 if segment_id in seen:
                     print_name_if_needed(file)
@@ -28,3 +18,18 @@ if __name__ == '__main__':
                         next_id = None
                     print(f'{segment_id} is duplicate, prev={prev_id}, next={next_id}')
                 seen.add(segment_id)
+
+if __name__ == '__main__':
+    files = sorted(Path('./html').glob('**/*.html'))
+
+    for file in files:
+        with file.open('r') as f:
+            lines = f.readlines()
+
+
+        segment_ids = [regex.findall(r'data-uid="[^:]+\:([a-z0-9.^-]+)"', line) for line in lines]
+        segment_ids = [s for s in segment_ids if s]
+
+        check_ordering(segment_ids, file)
+        
+        
