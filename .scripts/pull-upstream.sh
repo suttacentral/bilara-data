92acd8389b9d867e817188cbb79f0822e5a393f8
Karl Lew <karl@firepick.org>
2019-10-13 16:17:14 -0700

    pul

--- .scripts/pull-upstream.sh
+++ .scripts/pull-upstream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Refreshing bilara-data fork from upstream master..."
-git pull https://github.com/suttacentral/bilara-data
+git pull -Xtheirs https://github.com/suttacentral/bilara-data
