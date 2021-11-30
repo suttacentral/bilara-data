643b083d4f7cdde93e513a562357c04650359da5
Blake Walsh <blakeofthepath@gmail.com>
2021-11-30 08:35:11 +0100

    Add tests for correct reference wrangling

--- .nilakkhana/test_nilakkhana.py
+++ .nilakkhana/test_nilakkhana.py
@@ -35,11 +35,20 @@ def test_basic_transform(string, expected):
     [
         ('A [hyperlink](https://example.com)', "A <a href='https://example.com'>hyperlink</a>"),
         ('Best dhamma site = [](https://suttacentral.net)', "Best dhamma site = <a href='https://suttacentral.net'>https://suttacentral.net</a>"),
-        ('Text reference: [pli-tv-bu-vb-pj3]()', "Text reference: <a href='/pli-tv-bu-vb-pj3'>pli-tv-bu-vb-pj3</a>"),
-        ('Uncanonical reference: [](foobar)', "Uncanonical reference: <a href='/foobar'>foobar</a>"),
-        ('[Murder](pli-tv-bu-vb-pj3) is bad.', "<a href='/pli-tv-bu-vb-pj3'>Murder</a> is bad."),
     ]
 )
 def test_hyperlink_transform(string, expected):
+    assert transform(string) == expected
+    
+@pytest.mark.parametrize(
+    "string,expected",
+    [
+        ('comment-en-brahmali', 'Text reference: [pli-tv-bu-vb-pj3]()', "Text reference: <a href='/pli-tv-bu-vb-pj3/en/brahmali'>Pli-Tv-Bu-Vb-Pj3</a>"),
+        ('comment-en-sujato', '[Murder](pli-tv-bu-vb-pj3) is bad.', "<a href='/pli-tv-bu-vb-pj3/en/brahmali'>Murder</a> is bad."),
+        ('comment-en-sujato', 'Uppercasing [sn1.1]()', "<a href='/sn1.1/en/sujato'>SN 1.1</a>"),
+        ('comment-en-sujato', 'Colons hashed [sn1.1:1.7]()', "<a href='/sn1.1/en/sujato#1.7'>SN 1.1:1.7</a>"),
+    ]
+)
+def test_reference_transform(string, expected):
     assert transform(string) == expected
     
