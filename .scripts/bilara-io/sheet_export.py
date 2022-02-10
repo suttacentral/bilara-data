f33b97a70fcc6d12595f3f288c4db8e1dd6c78fb
Blake Walsh <blakeofthepath@gmail.com>
2022-02-10 12:19:46 +0100

    Updated bilara-io to seperate bulk of code into importable functions

--- .scripts/bilara-io/sheet_export.py
+++ .scripts/bilara-io/sheet_export.py
@@ -1,24 +1,12 @@
 #!/usr/bin/env python3
 
 import re
-import json
+import csv
+import pathlib
 import argparse
-import sys
-sys.path.append('..')
+from get_data import get_data
 
-
-from common import iter_json_files, repo_dir, bilarasortkey, json_load, bcolors
-
-def muid_sort_key(string):
-    if string.startswith('root'):
-        return (0, string)
-    elif string.startswith('translation'):
-        return (1, string)
-    elif string.startswith('markup'):
-        return (2, string)
-    else:
-        return (3, string)
-
+repo_dir = pathlib.Path(__file__).resolve().parent.parent.parent
 
 def save_sheet(rows, filename):
     
@@ -42,39 +30,7 @@ def save_sheet(rows, filename):
     else:
         import pyexcel
         pyexcel.isave_as(array=rows, dest_file_name=args.out or (','.join(args.uid) + '.ods'))
-
-
-
-def yield_rows(muid_strings, file_uid_mapping):
-    fields = ['segment_id'] + muid_strings
-    yield fields
-
-    field_mapping = {field:i for i, field in enumerate(fields)}
-    
-    for file_num, (uid, file_mapping) in enumerate(sorted(file_uid_mapping.items(), key=bilarasortkey)):
-        data = {}
-        segment_ids = set()
-        for muid_string in muid_strings:
-            if muid_string in file_mapping:
-                file = file_mapping[muid_string]
-                
-                try:
-                    file_data = json_load(file)
-                except json.decoder.JSONDecodeError:
-                    exit(1)
-                
-                i = field_mapping[muid_string]
-                for segment_id, value in file_data.items():
-                    if segment_id not in data:
-                        data[segment_id] = [segment_id] + [''] * (len(fields) - 1)
-                    data[segment_id][i] = value
         
-        for segment_id in sorted(data.keys(), key=bilarasortkey):
-            yield data[segment_id]
-        
-        if file_num < len(file_uid_mapping) - 1:
-            yield [''] * len(fields)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Export Spreadsheet")
     parser.add_argument('uid', nargs='+', help='One or more sutta UID to export')
@@ -92,54 +48,10 @@ if __name__ == '__main__':
         exclude_filter = frozenset(args.exclude.split(','))
     else:
         exclude_filter = None
-
-    file_uid_mapping = {}
-    for file in iter_json_files():
-        
-        
-        uid, muids_string = file.stem.split('_')
-
-        if not (uid in uids or any(part in uids for part in file.parent.parts)):
-            continue
-            
-        print('Reading {}'.format(str(file.relative_to(repo_dir))))
-
-        muids = frozenset(muids_string.split('-'))
-        if include_filter:
-            for muid in include_filter:
-                if isinstance(muid, frozenset):
-                    if muids.intersection(muid) == muid:
-                        break
-                else:
-                    if muid in muids:
-                        break
-            else:
-                continue
-        
-        if exclude_filter and exclude_filter.intersection(muids):
-            continue
-        
-        if uid not in file_uid_mapping:
-            file_uid_mapping[uid] = {}
-        file_uid_mapping[uid][muids_string] = file
     
-    if not file_uid_mapping:
-        print('No matches for {}'.format(",".join(args.uid)), file=sys.stderr)
-        exit(1)
-        
-    muid_strings = set()
-    for keys in file_uid_mapping.values():
-        muid_strings.update(keys)
-    
-    muid_strings = sorted(muid_strings, key=muid_sort_key)
-    
-    rows = yield_rows(muid_strings, file_uid_mapping)
+    rows = get_data(repo_dir, uids, include_filter, exclude_filter)
 
     save_sheet(rows, args.out)
-
-        
-        
-
 
 
 
